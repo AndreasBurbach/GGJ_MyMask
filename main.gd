c08a96bfdd67fe1612c68c07e65cbe542b62ddc4
Kay Park <park.kajus@gmail.com>
2026-01-31 14:39:25 +0100

    selection individual

--- main.gd
+++ main.gd
@@ -1,10 +1,11 @@
 extends Node
 
-@onready var coursorObj = $CourserObj
+@onready var coursorObj = $CourserLayer/CourserObj
 
 var selectedObj: Area2D
 var cursorOffset: Vector2 = Vector2(0,10)
 var name2item: Dictionary[String,Area2D]
+var selected = false
 
 enum Test {
 	Mouse
@@ -26,16 +27,23 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("Escape"):
 		selectedObj.show() 
 		coursorObj.hide()
-		#selectedObj = null
+		selected = false
 	var x = Vector2(coursorObj.texture.get_width()/2, coursorObj.texture.get_height() / 2)
 	coursorObj.position = get_viewport().get_mouse_position() + x
 	pass
 
+func interaction(a1:Area2D,a2:Area2D) -> void: 
+	print("use ", a1.name, " on ", a2.name)
 
 func _on_placeholder_selected(name: String,texture:Texture) -> void:
+	if selected: 
+		var other = name2item[name] 
+		interaction(selectedObj,other)
+		return
 	print(name )
 	selectedObj = name2item[name]
 	selectedObj.hide() 
 	coursorObj.texture = texture
 	coursorObj.show()
+	selected = true
 	pass # Replace with function body.
