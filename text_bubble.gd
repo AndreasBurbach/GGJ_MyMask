56accd603551e7fc7a96859173fb50aa81aa0b4d
Kay Park <park.kajus@gmail.com>
2026-01-31 18:53:14 +0100

    fix: removed test code

--- text_bubble.gd
+++ text_bubble.gd
@@ -5,7 +5,6 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void: 
 	hide()
-	show_for("Hello",5)
 	$Label.text= default
 	pass # Replace with function body.
 
